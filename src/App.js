82c9e76d5fa7f098a722abe632cd3aaedccbfb87
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-11-07 22:22:25 +0530

    added long and short term buttons

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [inputValue, setInputValue] = useState('$ ');
+  const [inputValue, setInputValue] = useState('$');
+  const [inputValue2, setInputValue2] = useState('$');
+  const [inputValue3, setInputValue3] = useState('$');
 
-  const [price,setPrice]=useState(0);
+  const [purchase_price,setPurchase_price]=useState(0);
+  const [sale_price,setSale_price]=useState(0);
+  const [expenses,setExpenses]=useState(0);
+  const [type,setType]=useState('short');
+  const [annual,setAnnual]=useState(0);
 
-  const handleInputChange = (event) => {
+  const handleInputChangePurchase = (event) => {
     const numericValue = event.target.value.replace(/[^0-9]/g, '');
-    setPrice(numericValue);
+    setPurchase_price(numericValue);
     setInputValue('$ ' + numericValue);
   };
+  const handleInputChangeSale = (event) => {
+    const numericValue = event.target.value.replace(/[^0-9]/g, '');
+    setSale_price(numericValue);
+    setInputValue2('$ ' + numericValue);
+  };
+
+  const handleInputChangeExpenses = (event) => {
+    const numericValue = event.target.value.replace(/[^0-9]/g, '');
+    setExpenses(numericValue);
+    setInputValue3('$ ' + numericValue);
+  };
+
+  const tickCSS={color: '#0141CF' , border: '1px solid #0141CF'};
+
+  const taxRate=[
+    '0%',
+    'Nil + 19% of excess over $18,200',
+    '$5,092 + 32.5% of excess over $45,000',
+    '$29,467 + 37% of excess over $120,000',
+    '$51,667 + 45% of excess over $180,000',
+  ]
 
 
   return (
@@ -56,7 +83,7 @@ function App() {
               id='block'
               type="text"
               value={inputValue}
-              onChange={handleInputChange}
+              onChange={handleInputChangePurchase}
             />
           </div>
         </div>
@@ -66,9 +93,102 @@ function App() {
               <p>Enter sale price of Crypto</p>
             </div>
             <div id='block-div'>
-              <input id='block'>
+              <input
+                id='block'
+                type="text"
+                value={inputValue2}
+                onChange={handleInputChangeSale}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
 
-              </input>
+      <div className='row2'>
+        <div className='left'>
+          <div id='text'>
+            <p>Enter your Expenses</p>
+          </div>
+          <div id='block-div'>
+            <input
+              id='block'
+              type="text"
+              value={inputValue3}
+              onChange={handleInputChangeExpenses}
+            />
+          </div>
+        </div>
+        <div className='right'>
+          <div className='inner-right'>
+            <div id='text'>
+              <p>Investment Type</p>
+            </div>
+            <div id='block-div'>
+              <div id='select-box' style={type=='short'?tickCSS:null} onClick={()=>{setType('short');}}>
+                <p>Short Term {type=='short'?<p id='tick'>✔</p>:null}</p>
+              </div>
+              <div id='select-box' style={type=='long'?tickCSS:null} onClick={()=>{setType('long');}}>
+                <p>Long Term {type=='long'?<p id='tick'>✔</p>:null}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='row2'>
+        <div className='left'>
+          <div id='text'>
+            <p>Select Your Annual Income</p>
+          </div>
+          <div id='block-div'>
+            <select id='block' onChange={(e)=>{setAnnual(e.target.value)}}>
+              <option value="0">$0 - $18,200</option>
+              <option value="1">$18,201 - $45,000</option>
+              <option value="2">$45,001 - $120,000</option>
+              <option value="3">$120,001 - $180,000</option>
+              <option value="4">$180,001+</option>
+            </select>
+          </div>
+        </div>
+        <div className='right'>
+          <div className='inner-right'>
+            <div id='text'>
+              <p>Tax Rate</p>
+            </div>
+            <div id='block-div'>
+              <div id='block-rate'>
+                <p>{taxRate[annual]}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='row2'>
+        <div className='left'>
+          <div id='text'>
+            <p>Capital gains amount</p>
+          </div>
+          <div id='block-div'>
+            <input
+              id='block'
+              type="text"
+              value={sale_price-expenses-purchase_price}
+              onChange={handleInputChangePurchase}
+            />
+          </div>
+        </div>
+        <div className='right'>
+          <div className='inner-right'>
+            <div id='text'>
+              <p>Discount for long term gains</p>
+            </div>
+            <div id='block-div'>
+              <input
+                id='block'
+                type="text"
+                value={inputValue2}
+                onChange={handleInputChangeSale}
+              />
             </div>
           </div>
         </div>
